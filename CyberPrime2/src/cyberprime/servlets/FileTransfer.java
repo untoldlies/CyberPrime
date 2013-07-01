fbeb334bda330ddf9ea6cf9aaa71b5147933c393
tanwk547 <Tan Wai Kit@tnwaikit@hotmail.com>
2013-07-01 16:27:15 +0800

    added database script

--- CyberPrime2/src/cyberprime/servlets/FileTransfer.java
+++ CyberPrime2/src/cyberprime/servlets/FileTransfer.java
@@ -9,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.JProgressBar;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
@@ -17,6 +19,8 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.apache.commons.io.output.*;
+
+import cyberprime.servlets.ProgressBarServlet.Task;
 
 @WebServlet("/FileTransfer")
 public class FileTransfer extends HttpServlet {
@@ -28,7 +32,9 @@ public class FileTransfer extends HttpServlet {
 	private int maxMemSize = 1000000 * 1000;
 	private File file;
 	private String Id = null;
-	private String ID = "waikit";
+	private String ID = "tnwaikit";
+	private JProgressBar progressBar;
+	private Task task;
 
 	public void init() {
 		// Get the file location where it would be stored.
@@ -54,6 +60,12 @@ public class FileTransfer extends HttpServlet {
 		isMultipart = ServletFileUpload.isMultipartContent(request);
 		response.setContentType("text/html");
 
+		TestProgressListener testProgressListener = new TestProgressListener();
+		upload.setProgressListener(testProgressListener);
+
+		HttpSession session = request.getSession();
+		session.setAttribute("testProgressListener", testProgressListener);
+
 		if (!isMultipart) {
 			out.println("<html>");
 			out.println("<head>");
@@ -66,8 +78,8 @@ public class FileTransfer extends HttpServlet {
 			out.println("</html>");
 			return;
 		}
-		
-		else{
+
+		else {
 
 			out.println("<html>");
 			out.println("<head>");
@@ -82,8 +94,6 @@ public class FileTransfer extends HttpServlet {
 		try {
 
 			if (repo != null) {
-
-				System.out.println("Repository: " + repo.getAbsolutePath());
 
 				RequestContext req = new ServletRequestContext(request);
 				// Parse the request to get file items.
@@ -106,32 +116,36 @@ public class FileTransfer extends HttpServlet {
 
 					else {
 						try {
-							System.out.println("Available");
 
-								if (!item.isFormField()) {
-									// Get the uploaded file parameters
-									
-									String fileName = item.getName();
+							if (!item.isFormField()) {
+								// Get the uploaded file parameters
 
-									// Write the file
-									if (fileName.lastIndexOf("\\") >= 0) {
-										file = new File(filePath
-												+ fileName.substring(fileName
-														.lastIndexOf("\\")));
-									} else {
-										file = new File(filePath
-												+ fileName.substring(fileName
-														.lastIndexOf("\\") + 1));
-									}
-									out.println("<p><strong>Thank You For Waiting</strong></p>");
-									item.write(file);
-									out.println("Uploaded Filename: "
-											+ fileName + "<br>");
-									System.out.println("printed");
+								String fileName = item.getName();
+
+								// Write the file
+								if (fileName.lastIndexOf("\\") >= 0) {
+									file = new File(filePath
+											+ fileName.substring(fileName
+													.lastIndexOf("\\")));
+								} else {
+									file = new File(filePath
+											+ fileName.substring(fileName
+													.lastIndexOf("\\") + 1));
 								}
-							
+								out.println("<p><strong>Thank You For Waiting</strong></p>");
+								item.write(file);
+								out.println("Uploaded Filename: " + fileName
+										+ "<br>");
+								System.out.println("printed");
+							}
+
 							out.println("</body>");
 							out.println("</html>");
+							
+/*							if (file upload == true) {
+ * 								send notification to the receiver
+							};
+*/
 						} catch (Exception ex) {
 							out.println("<p><strong>No File Found, Please Try Again</strong></p>");
 						}
@@ -142,8 +156,6 @@ public class FileTransfer extends HttpServlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
