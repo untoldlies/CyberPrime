cf9e795bb28f7054212df937526e8db4e215cb9e
Samuel Ong <Samuel Ong@SamuelOng-PC>
2013-08-01 14:17:55 +0800

    Changed samuel's constant path

--- CyberPrime2/src/cyberprime/util/Constants.java
+++ CyberPrime2/src/cyberprime/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	//Change your path accordingly to your login Image repos for registering and login purposes.
 	public static String DEANE_PATH = "C:\\Users\\Deane\\git\\CyberPrime2\\CyberPrime2\\WebContent\\loginImages\\";
 	public static String WAIKIT_PATH = "C:\\Users\\Tan Wai Kit\\Desktop\\CyberPrime\\CyberPrime2\\WebContent\\loginImages\\";
-	public static String SAMUEL_PATH = "C:\\Users\\Samuel Ong\\git\\CyberPrime\\CyberPrime2\\WebContent\\loginImages";
+	public static String SAMUEL_PATH = "C:\\Users\\Samuel Ong\\git\\CyberPrime\\CyberPrime2\\WebContent\\loginImages\\";
 	public static String NICHOLAS_PATH = "";
 	
 	public static String IP_ADDRESS = "localhost";
